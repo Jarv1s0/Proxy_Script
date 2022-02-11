52770ca5c7d422fec5c3a745dd9eb418802bd950
Jarviss <linkai0320@gmail.com>
2022-02-11 11:08:39 +0800

    Update weibo.js

--- weibo.js
+++ weibo.js
@@ -1,10 +1,7 @@
 const version = 'v0211.1';
-let storeMainConfig = $.read('mainConfig');
-const mainConfig = storeMainConfig ? JSON.parse(storeMainConfig) : {
-	isDebug: false,						//开启调试，会打印运行中部分日志
-	//个人中心配置，其中多数是可以直接在更多功能里直接移除
-	removeHomeVip: true,				//个人中心头像旁边的vip样式
-	removeHomeCreatorTask: true,		//个人中心创作者中心下方的轮播图
+
+const otherUrls = {
+	'/profile/me': 'removeHome',
 }
 const path1 = "/groups/timeline";
 const path2 = "/statuses/unread";
